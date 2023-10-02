e07d23948d8454a54a86495c88aa05707b42c624
teetee6 <teetee6@naver.com>
2023-10-02 21:16:26 +0900

    build/db 연결주소 개발서버->배포서버로 변경

--- lib/db.ts
+++ lib/db.ts
@@ -2,17 +2,17 @@ import { MongoClient } from 'mongodb';
 
 let cachedClient: MongoClient | null = null;
 
+const deployment_addr = `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PASSWORD}@cluster0.ns4kyuj.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`;
+const development_addr = `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PASSWORD}@atlascluster.whlisy6.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`;
+
 export async function connectToDatabase(): Promise<MongoClient> {
   if (cachedClient) {
     return cachedClient;
   }
 
-  const client = new MongoClient(
-    `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PASSWORD}@atlascluster.whlisy6.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`,
-    {
-      maxPoolSize: 10,
-    }
-  );
+  const client = new MongoClient(deployment_addr, {
+    maxPoolSize: 10,
+  });
 
   cachedClient = await client.connect();
   return cachedClient;
