124e67947e83caf2faf55f03e6d204df6d8092ca
teetee6 <teetee6@naver.com>
2023-10-03 09:52:34 +0900

    build/db 연결주소 .env주소로 의존성있게  변경

--- lib/db.ts
+++ lib/db.ts
@@ -5,12 +5,15 @@ let cachedClient: MongoClient | null = null;
 const deployment_addr = `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PASSWORD}@cluster0.ns4kyuj.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`;
 const development_addr = `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PASSWORD}@atlascluster.whlisy6.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`;
 
+const address =
+  process.env.NODE_ENV === 'production' ? deployment_addr : development_addr;
+
 export async function connectToDatabase(): Promise<MongoClient> {
   if (cachedClient) {
     return cachedClient;
   }
 
-  const client = new MongoClient(deployment_addr, {
+  const client = new MongoClient(address, {
     maxPoolSize: 10,
   });
 
